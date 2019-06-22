13ce8321d22d7a9ec19c124143a2afd463588847
dhananjay431 <dbel431@gmail.com>
2019-06-22 18:46:20 +0530

    show

--- src/index.js
+++ src/index.js
@@ -11,3 +11,26 @@
 // require("../js/now-ui-kit.js?v=1.1.0");
 // require("../js/aos.js");
 // require("../scripts/main.js");
+var message = new SpeechSynthesisUtterance("hello");
+var voices = speechSynthesis.getVoices();
+
+message.voice = voices[2];
+speechSynthesis.speak(message);
+/*
+var p = 3,s="";
+for(var i=1;i<=10;i++){
+s+=`${p} ${i} za ${p*i}  `;
+}
+console.log(s);
+function show(i,p){
+if(i==11) return;
+console.log(i);
+var message = new SpeechSynthesisUtterance(`${p} ${i} za ${p*i}`);
+var voices = speechSynthesis.getVoices();
+console.log(voices);
+message.voice = voices[2];
+speechSynthesis.speak(message);
+show(i+1);
+}
+show(1,3)
+*/
